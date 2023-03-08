76a4c7a7e0a917e809202b8a6cebd10ebfc9438b
彭昊旻 <haomin.peng@maxjia.com>
2023-03-08 16:32:19 +0800

    feat: index html

--- spider/game/urls.py
+++ spider/game/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('index', views.index, name='index'),
     path('game_list', views.game_list, name='game_list'),
     path('game_detail', views.game_detail, name='game_detail'),
 ]
--- spider/game/views.py
+++ spider/game/views.py
@@ -1,23 +1,57 @@
 import json
 import grpc
-
+import sys
+import requests
 from django.shortcuts import render
-# Create your views here.
-from django.http import HttpResponse,JsonResponse
-from django.views.decorators.csrf import csrf_exempt
 from .proto import game_pb2,game_pb2_grpc
 
-
+# Create your views here.
+from django.http import HttpResponse,JsonResponse
+from django.http.request import QueryDict
+from django.views.decorators.csrf import csrf_exempt
+from django.template import loader
+from loguru import logger
+logger.remove()
+logger.add(sys.stdout, level='INFO', format='{message}')
+logger.add('output.log', level='INFO', format='{message}')
 def index(request):
-    return HttpResponse("Hello, world. You're at the game index.")
-
-
+    if request.method.lower() == "get":
+        param_dict = request.GET
+        current_page = 1
+        if "page" in param_dict:
+            current_page = int(param_dict['page'])
+        keyword = ""
+        if "key_word" in param_dict:
+            keyword = param_dict['key_word']
+        # 请求接口
+        resp = requests.post("http://192.168.0.204:8059/game_store/game_list",data=param_dict)
+        data = resp.text
+        result = json.loads(data)
+        page_list = []
+        total = 0
+        if result['data'] is not None:
+            total = result['data'].get('total')
+        end = int((total / 10))
+        if total % 10 != 0:
+            end = end + 1
+        for i in range(1,end+1):
+            page_list.append(i)
+        template = loader.get_template('index.html')
+        logger.info(result)
+        context = {
+            'game_info': result['data'],
+            'language': result['language'],
+            'page': current_page,
+            'page_last': len(page_list),
+            'page_list': page_list,
+            'keyword': keyword,
+        }
+        return HttpResponse(template.render(context, request))
 
 @csrf_exempt
 def game_list(request):
     try:
-        data = request.body.decode("utf-8")
-        req = json.loads(data)
+        req = request.POST
         page = 0
         page_size = 0
         keyword = ""
@@ -27,6 +61,11 @@ def game_list(request):
             page_size = int(req["page_size"])
         if "key_word" in req:
             keyword = req["key_word"]
+        logger.info({
+            "page": page,
+            "page_size": page_size,
+            "key_word": keyword
+        })
         with grpc.insecure_channel("0.0.0.0:6781") as channel:
             stub = game_pb2_grpc.GameStub(channel)
             # 调用定义的SayHello方法
@@ -64,8 +103,7 @@ def game_list(request):
 @csrf_exempt
 def game_detail(request):
     try:
-        data = request.body.decode("utf-8")
-        req = json.loads(data)
+        req = request.POST
         name = req.get("game_name")
         with grpc.insecure_channel("0.0.0.0:6781") as channel:
             stub = game_pb2_grpc.GameStub(channel)
